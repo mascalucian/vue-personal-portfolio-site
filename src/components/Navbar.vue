85d12d67b002a89ad668eaf3078a75301c9ac0e3
Lucian Masca <lucianm0607@icloud.com>
2021-10-10 08:23:34 +0300

    Added: Navbar component

---
+++ src/components/Navbar.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="nav">
+        <div class="left">
+            <div id="logo" class="el">
+                Logo
+            </div>
+            <div id="home" class="el">
+                Home
+            </div>
+            <div id="projects" class="el">
+                Projects
+            </div>
+            <div id="contact" class="el">
+                Contact
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+}
+</script>
+
+<style scoped>
+.nav {
+    display: flex;
+    top:0;
+    background-color: black;
+}
+.left {
+    display: flex;
+}
+.el {
+    padding: 20px;
+    font-size: 20px;
+    color:white;
+}
+</style>
